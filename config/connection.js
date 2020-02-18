6760b256279e1371b2fd718e1726673e02e55eb6
Zayd Sarraj <zayd.sarraj@gmail.com>
2020-02-18 03:25:54 -0500

    added JawsDB connection

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,18 @@
 const mysql = require("mysql");
 
 // Setup connection
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
-});
+// JawsDB connection
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password',
+    database: 'burgers_db'
+  });
+}
 
 // Connect
 connection.connect(function(err) {
